af5ac6cc0c6c70d8e8c609db0aa26ba741ba5092
Kenjiro56 <hiroken506@gmail.com>
2021-04-16 23:51:31 +0900

    add/answers_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_144324) do
+ActiveRecord::Schema.define(version: 2021_04_16_144914) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.text "comment"
+    t.string "image"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.integer "user_id"
