f9d3db0aee114f957f23814d83626283346baf51
Kenjiro56 <hiroken506@gmail.com>
2021-04-17 01:36:16 +0900

    feature/cloudinary

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ require 'open-uri'
 require "sinatra/json"
 require './models/models.rb'
 require 'sinatra/activerecord'
-require 'pry'
+
+before do
+    Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name =ENV['CLOUC_NAME']
+        config.api_key = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    end
+end
 
 get '/' do
     "Hello World!"
