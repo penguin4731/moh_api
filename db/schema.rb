7559e1455b6caa5019dbb18c42f4aaf215174e42
Kenjiro56 <hiroken506@gmail.com>
2021-04-16 23:48:26 +0900

    add/questions_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_144055) do
+ActiveRecord::Schema.define(version: 2021_04_16_144324) do
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.text "comment"
+    t.string "image"
+    t.string "bestanswer_id"
+  end
 
   create_table "tips", force: :cascade do |t|
     t.integer "user_id"
