e18a10f2cf8010b7f9ede711e1d3cd4bb640409d
Kenjiro56 <hiroken506@gmail.com>
2021-04-16 23:52:45 +0900

    add/categories_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_144914) do
+ActiveRecord::Schema.define(version: 2021_04_16_145152) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id"
     t.integer "question_id"
     t.text "comment"
     t.string "image"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "questions", force: :cascade do |t|
