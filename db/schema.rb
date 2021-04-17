82b236a19d3d853f5b54ddf9da67adfd961346cc
Kenjiro56 <hiroken506@gmail.com>
2021-04-17 22:31:05 +0900

    drop/tips_replies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_123445) do
+ActiveRecord::Schema.define(version: 2021_04_17_132945) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id"
@@ -40,13 +40,6 @@ ActiveRecord::Schema.define(version: 2021_04_17_123445) do
     t.integer "user_id"
     t.text "comment"
     t.string "title"
-  end
-
-  create_table "tips_replies", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "tip_id"
-    t.text "comment"
-    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
